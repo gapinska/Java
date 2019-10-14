564ba10f9be75f525e2e9e59871557f84ba37030
gapinska <gapinska.mon@gmail.com>
2019-10-14 21:07:37 +0200

    methods in java

--- methods/Methods.java
+++ methods/Methods.java
@@ -7,6 +7,7 @@ public class Methods {
         hi("Mon!");
         hi("Chris!");
         hi("Mon!", "Chris!");
+        hi(" Mon,", " Chris,", " John!");
     }
 
     private static void sayHi() {
@@ -19,5 +20,13 @@ public class Methods {
 
     private static void hi(String name, String name2) {
         System.out.println("Hi " + name + ", " + "name2");
+    }
+
+    private static void hi(String... names) {
+        String result = "";
+        for (String s : names) {
+             result = result + s;
+        }
+        System.out.println("Hi"+ result);
     }
 }
