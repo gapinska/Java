21f785e1f814d61e2061c1c0b3979bee48750164
gapinska <gapinska.mon@gmail.com>
2019-10-14 20:44:21 +0200

    refactor

--- Main.java
+++ Main.java
@@ -3,10 +3,9 @@ package pl.monikagapinska.firstprogramme;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
         int menu = 10;
-
-
-        var even = (menu % 2== 0) ? "yes" : "no";
+        var even = (menu % 2 == 0) ? "yes" : "no";
         System.out.println(even);
     }
 }
