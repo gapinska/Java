75b4a30c0216200da3bba2768fce5c49982d9005
gapinska <gapinska.mon@gmail.com>
2019-10-27 08:56:14 +0100

    class and object

--- classandobject/Address.java
+++ classandobject/Address.java
@@ -3,4 +3,20 @@ package pl.monikagapinska.firstprogramme.classandobject;
 public class Address {
     private String postOffice;
     private String postCode;
+
+    public String getPostOffice() {
+        return postOffice;
+    }
+
+    public void setPostOffice(String postOffice) {
+        this.postOffice = postOffice;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
 }
--- classandobject/Test.java
+++ classandobject/Test.java
@@ -2,7 +2,17 @@ package pl.monikagapinska.firstprogramme.classandobject;
 
 public class Test {
     public static void main(String[] args) {
-        Client client1 = new Client();
-        client1.name = "Christian";
+//        Client client1 = new Client();
+//        client1.name = "Christian";
+
+        Client clientMichael = new Client ();
+
+        Address michaelAddress = new Addresss();
+        michaelAddress.setPostCode("00-001");
+        michaelAddress.setPostOffice("Warsaw");
+
+        clientMichael.setFirstName("Michael");
+        clientMichael.setLastName("Forbes");
+        clientMichael.setAddress(michaelAddress);
     }
 }
