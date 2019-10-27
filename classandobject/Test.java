daad531e1fa31befeb55daa30702264932754964
gapinska <gapinska.mon@gmail.com>
2019-10-27 08:59:54 +0100

    class and object

--- classandobject/Test.java
+++ classandobject/Test.java
@@ -7,12 +7,14 @@ public class Test {
 
         Client clientMichael = new Client ();
 
-        Address michaelAddress = new Addresss();
+        Address michaelAddress = new Address();
         michaelAddress.setPostCode("00-001");
         michaelAddress.setPostOffice("Warsaw");
 
         clientMichael.setFirstName("Michael");
         clientMichael.setLastName("Forbes");
         clientMichael.setAddress(michaelAddress);
+
+        System.out.println(clientMichael.getLastName());
     }
 }
