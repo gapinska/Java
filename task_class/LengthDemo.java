30702a9628e29d6119844602e76793d4a389cc5a
gapinska <gapinska.mon@gmail.com>
2019-11-17 10:40:00 +0100

    class task in java

---
+++ task_class/LengthDemo.java
@@ -0,0 +1,8 @@
+package pl.monikagapinska.firstprogramme.task_class;
+
+public class LengthDemo {
+    public static void main(String[] args) {
+        Rectangle box = new Rectangle();
+        box.setLength(10.0);
+    }
+}
