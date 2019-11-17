557e2c2524c5ef272de376a3a53a6e34be0bc9e4
gapinska <gapinska.mon@gmail.com>
2019-11-17 09:39:43 +0100

    class task in java

---
+++ task_class/Rectangle.java
@@ -0,0 +1,10 @@
+package pl.monikagapinska.firstprogramme.task_class;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public void setLength(double len) {
+        length = len;
+    }
+}
