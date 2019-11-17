2f6cf19b600dad1c5d178e967d57f52a034a0185
gapinska <gapinska.mon@gmail.com>
2019-11-17 11:00:41 +0100

    class task in java

--- task_class/LengthWidthDemo.java
+++ task_class/LengthWidthDemo.java
@@ -1,8 +1,12 @@
 package pl.monikagapinska.firstprogramme.task_class;
 
-public class LengthDemo {
+public class LengthWidthDemo {
     public static void main(String[] args) {
         Rectangle box = new Rectangle();
         box.setLength(10.0);
+        box.setWidth(20.0);
+        double sizeA=box.getLength();
+        double sizeB=box.getWidth();
+        double area = box.getArea();
     }
 }
--- task_class/Rectangle.java
+++ task_class/Rectangle.java
@@ -7,4 +7,20 @@ public class Rectangle {
     public void setLength(double len) {
         length = len;
     }
+
+    public void setWidth(double wid) {
+        width = wid;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getArea (){
+        return length*width;
+    }
 }
